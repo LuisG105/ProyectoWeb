2d8e6bb3bcb26d4c75e9b324e924009ff13e8efd
LuisG105 <luis.gutierrez105@inacapmail.cl>
2026-09-07 12:22:40 -0300

    Cambios de html

--- estaticas/views.py
+++ estaticas/views.py
@@ -4,11 +4,4 @@ def mostrar_home(request):
     return render(request, 'inicio.html')
 
 def mostrar_acerca(request):
-    datos = {
-        'nombre': 'Eduardo Molina',
-        'correo': 'EduMoli04@gmail.com',
-        'redes_sociales': [
-            {'plataforma': 'LinkedIn', 'url': 'https://linkedin.com'},
-        ]
-    }
-    return render(request, 'acerca.html', {'perfil': datos})
+    return render(request, 'acerca.html',)
