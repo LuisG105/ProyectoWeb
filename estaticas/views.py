65f1fded849d2a62edb87b17b151fbd2dc39fbab
LuisG105 <luis.gutierrez105@inacapmail.cl>
2026-09-07 12:54:04 -0300

    Cambios de html

--- estaticas/views.py
+++ estaticas/views.py
@@ -9,10 +9,6 @@ def mostrar_acerca(request):
         'carrera': 'Turismo',
         'correo': 'EnrMoli050@gmail.com',
         'region': 'Calama, Chile',
-        'redes_sociales': [
-            {'plataforma': 'LinkedIn', 'url': 'https://linkedin.com'},
-            {'plataforma': 'GitHub', 'url': 'https://github.com/LuisG105'},
-        ]
     }
 
     return render(request, 'acerca.html', {'perfil': perfil_datos})
